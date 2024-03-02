e113cb9b78bdfce5529423e21fb8458c97f6f9aa
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2024-03-01 22:10:33 -0300

    feat: add default btn

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,14 @@
+type Styles = {
+    defaultStyles: React.CSSProperties
+  }
+
+const styles: Styles = {
+    defaultStyles: {
+        padding: '8px 24px',
+        borderRadius: 6,
+        border: 'none',
+    }
+    
+  }
+  
+  export default styles
