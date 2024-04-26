8a7117841f7ac8802d159c521531ac1e6c34b11e
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2024-04-26 18:17:59 -0300

    feat: button improvements

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { CSSProperties, ReactNode, forwardRef } from 'react'
 import styles from './styles'
-import { ButtonProps } from './types'
 
-
-
-const Button:React.FC<ButtonProps> = ({label,style,onClick,type}) => {
-  return (
-    <button type={type} style={{...styles.defaultStyles,...style}} onClick={onClick}>{label}</button>
-  )
+type ButtonProps = {
+    label: ReactNode
+    onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    style?: CSSProperties
+    type?: "button" | "submit" | "reset"
 }
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ label, style, onClick, type }, ref) => {
+      return (
+          <button
+              ref={ref}
+              type={type}
+              style={{ ...styles.defaultStyles, ...style }}
+              onClick={onClick}
+          >
+              {label}
+          </button>
+      )
+  }
+)
 
 export default Button
