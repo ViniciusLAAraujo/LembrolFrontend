7531909fa6ec0e93605389fc7e9f1d77e9343f85
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2024-05-27 22:13:53 -0300

    feat: add func to dateUtils

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,2 +1,16 @@
 
-export const daysOfWeekSimplify = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"]
+export const daysOfWeekSimplify = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+export function daysStringSlash(days:number[]):string
+{ 
+    var result:string = ""
+    days.forEach((day, index) => {
+        if (day >= 0 && day < 7) {
+            if(index === 0)
+                result += (daysOfWeekSimplify[day]) 
+            else
+                result += (`/${daysOfWeekSimplify[day]}`)
+        }
+    })
+    return result
+}
