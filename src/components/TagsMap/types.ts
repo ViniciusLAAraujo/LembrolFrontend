d7cdeb4ebb594ca19a0872d4b961a98d0d0eabaf
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2024-05-27 22:25:06 -0300

    fix: add tagMap comp to Gps page

--- src/components/TagsMap/types.ts
+++ src/components/TagsMap/types.ts
@@ -2,7 +2,7 @@ import { LatLngTuple } from "leaflet"
 import { Tag } from "../../pages/Gps/types"
 
 export type TagsMapProps = {
-    selectedTag : Tag
+    selectedTag : Tag | null
     mapPosition : LatLngTuple | undefined
     tags : Tag[]
 }
